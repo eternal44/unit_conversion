3ce2b5d15050ef42b90846ed76d6534bc604d0a2
James Youn <jamesyoun710@gmail.com>
2015-10-02 19:02:21 -0700

    Temperature conversions for all current combos

--- lib/unit_conversion.rb
+++ lib/unit_conversion.rb
@@ -33,6 +33,30 @@ class UnitConversion
     @measurement * 1.8
   end
 
+  def celcius_to_fahrenheit
+    @measurement * 9/5 + 32.0
+  end
+
+  def fahrenheit_to_celcius
+    (@measurement - 32.0) * 5/9
+  end
+
+  def celcius_to_rankine
+    (@measurement + 273.15) * 9/5
+  end
+
+  def rankine_to_celcius
+    (@measurement - 491.67) * 5/9
+  end
+
+  def rankine_to_fahrenheit
+    @measurement - 459.67
+  end
+
+  def fahrenheit_to_rankine
+    @measurement + 459.67
+  end
+
   ########################
   # DISTANCE CONVERSIONS #
   ########################
--- spec/unit_conversion_spec.rb
+++ spec/unit_conversion_spec.rb
@@ -1,6 +1,10 @@
 require 'spec_helper'
 
 describe UnitConversion do
+  ###########################
+  # TEMPERATURE CONVERSIONS #
+  ###########################
+
   temperature_subject = UnitConversion.new(0)
 
   it '#celcius_to_kelvin' do
@@ -25,6 +29,31 @@ describe UnitConversion do
 
   it '#rankine_to_kelvin' do
     expect(temperature_subject.rankine_to_kelvin).to eq 0
+  end
+
+  #
+  it '#celcius_to_fahrenheit' do
+    expect(temperature_subject.celcius_to_fahrenheit).to eq 32
+  end
+
+  it 'fahrenheit_to_celcius' do
+    expect(temperature_subject.fahrenheit_to_celcius.round(2)).to eq -17.78
+  end
+
+  it 'celcius_to_rankine' do
+    expect(temperature_subject.celcius_to_rankine.round(2)).to eq 491.67
+  end
+
+  it 'rankine_to_celcius' do
+    expect(temperature_subject.rankine_to_celcius).to eq -273.15
+  end
+
+  it 'rankine_to_fahrenheit' do
+    expect(temperature_subject.rankine_to_fahrenheit).to eq -459.67
+  end
+
+  it 'fahrenheit_to_rankine' do
+    expect(temperature_subject.fahrenheit_to_rankine).to eq 459.67
   end
 
   ########################
