68768de8aaba76e57e4daa8ab8eea6b16d92c36e
James Youn <jamesyoun710@gmail.com>
2015-10-05 14:40:36 -0700

    Use temperature proxies

--- lib/unit_conversion/temperature_conversion.rb
+++ lib/unit_conversion/temperature_conversion.rb
@@ -1,50 +1,59 @@
 module TemperatureConversion
-  def celcius_to_kelvin
-    @measurement + 273.15
+  # Proxy = Kelvin
+  def celcius_to_kelvin(value)
+    value += 273.15
   end
 
-  def fahrenheit_to_kelvin
-    (@measurement + 459.67) * 5 / 9
+  def kelvin_to_celcius(value)
+    value -= 273.15
   end
 
-  def rankine_to_kelvin
-    @measurement / 1.8
+  def fahrenheit_to_kelvin(value)
+    (value + 459.67) * 5 / 9
   end
 
-  def kelvin_to_celcius
-    @measurement - 273.15
+  def kelvin_to_fahrenheit(value)
+    (value * 9 / 5.0) - 459.67
   end
 
-  def kelvin_to_fahrenheit
-    (@measurement * 9 / 5.0) - 459.67
+  def rankine_to_kelvin(value)
+    value / 1.8
   end
 
-  def kelvin_to_rankine
-    @measurement * 1.8
+  def kelvin_to_rankine(value)
+    value * 1.8
   end
 
-  def celcius_to_fahrenheit
-    @measurement * 9/5 + 32.0
+  # Requests
+
+  def celcius_to_fahrenheit(value)
+    in_kelvin = celcius_to_kelvin(value)
+    kelvin_to_fahrenheit(in_kelvin)
   end
 
-  def fahrenheit_to_celcius
-    (@measurement - 32.0) * 5/9
+  def fahrenheit_to_celcius(value)
+    in_kelvin = fahrenheit_to_kelvin(value)
+    kelvin_to_celcius(in_kelvin)
   end
 
-  def celcius_to_rankine
-    (@measurement + 273.15) * 9/5
+  def celcius_to_rankine(value)
+    in_kelvin = celcius_to_kelvin(value)
+    kelvin_to_rankine(in_kelvin)
   end
 
-  def rankine_to_celcius
-    (@measurement - 491.67) * 5/9
+  def rankine_to_celcius(value)
+    in_kelvin = rankine_to_kelvin(value)
+    kelvin_to_celcius(in_kelvin)
   end
 
-  def rankine_to_fahrenheit
-    @measurement - 459.67
+  def rankine_to_fahrenheit(value)
+    in_kelvin = rankine_to_kelvin(value)
+    kelvin_to_fahrenheit(in_kelvin)
   end
 
-  def fahrenheit_to_rankine
-    @measurement + 459.67
+  def fahrenheit_to_rankine(value)
+    in_kelvin = fahrenheit_to_kelvin(value)
+    kelvin_to_rankine(in_kelvin)
   end
 end
 
--- spec/unit_conversion_spec/temperature_spec.rb
+++ spec/unit_conversion_spec/temperature_spec.rb
@@ -1,58 +1,62 @@
 require 'spec_helper'
 
-describe UnitConversion do
-  ###########################
-  # TEMPERATURE CONVERSIONS #
-  ###########################
-
+describe TemperatureConversion do
   temperature_subject = UnitConversion.new(0)
+  measurement = temperature_subject.measurement
+
+  ###########
+  # PROXIES #
+  ###########
 
   it '#celcius_to_kelvin' do
-    expect(temperature_subject.celcius_to_kelvin).to eq 273.15
-  end
-
-  it '#fahrenheit_to_kelvin' do
-    expect(temperature_subject.fahrenheit_to_kelvin).to eq 255.3722222222222
+    expect(temperature_subject.celcius_to_kelvin(measurement).round(2)).to eq 273.15
   end
 
   it '#kelvin_to_celcius' do
-    expect(temperature_subject.kelvin_to_celcius).to eq -273.15
+    expect(temperature_subject.kelvin_to_celcius(measurement).round(2)).to eq -273.15
+  end
+
+  it '#fahrenheit_to_kelvin' do
+    expect(temperature_subject.fahrenheit_to_kelvin(measurement).round(2)).to eq 255.37
   end
 
   it '#kelvin_to_fahrenheit' do
-    expect(temperature_subject.kelvin_to_fahrenheit).to eq -459.67
-  end
-
-  it '#kelvin_to_rankine' do
-    expect(temperature_subject.kelvin_to_rankine).to eq 0
+    expect(temperature_subject.kelvin_to_fahrenheit(measurement).round(2)).to eq -459.67
   end
 
   it '#rankine_to_kelvin' do
-    expect(temperature_subject.rankine_to_kelvin).to eq 0
+    expect(temperature_subject.rankine_to_kelvin(measurement).round(2)).to eq 0
   end
 
-  #
+  it '#kelvin_to_rankine' do
+    expect(temperature_subject.kelvin_to_rankine(measurement).round(2)).to eq 0
+  end
+
+  ############
+  # REQUESTS #
+  ############
+
   it '#celcius_to_fahrenheit' do
-    expect(temperature_subject.celcius_to_fahrenheit).to eq 32
+    expect(temperature_subject.celcius_to_fahrenheit(measurement).round(2)).to eq 32
   end
 
   it '#fahrenheit_to_celcius' do
-    expect(temperature_subject.fahrenheit_to_celcius.round(2)).to eq -17.78
+    expect(temperature_subject.fahrenheit_to_celcius(measurement).round(2)).to eq -17.78
   end
 
   it '#celcius_to_rankine' do
-    expect(temperature_subject.celcius_to_rankine.round(2)).to eq 491.67
+    expect(temperature_subject.celcius_to_rankine(measurement).round(2)).to eq 491.67
   end
 
   it '#rankine_to_celcius' do
-    expect(temperature_subject.rankine_to_celcius).to eq -273.15
+    expect(temperature_subject.rankine_to_celcius(measurement).round(2)).to eq -273.15
   end
 
   it '#rankine_to_fahrenheit' do
-    expect(temperature_subject.rankine_to_fahrenheit).to eq -459.67
+    expect(temperature_subject.rankine_to_fahrenheit(measurement).round(2)).to eq -459.67
   end
 
   it '#fahrenheit_to_rankine' do
-    expect(temperature_subject.fahrenheit_to_rankine).to eq 459.67
+    expect(temperature_subject.fahrenheit_to_rankine(measurement).round(2)).to eq 459.67
   end
 end
