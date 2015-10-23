a2839af1c7660a21a531f1ba14b4e2951881900a
James Youn <jamesyoun710@gmail.com>
2015-10-23 15:49:21 -0700

    WIP Bugfix:  fix pathing to require distance in test
    My distance formula is still wrong.

--- spec/distance_spec.rb
+++ spec/distance_spec.rb
@@ -1,6 +1,8 @@
 require 'spec_helper'
-require 'distance'
+require 'unit_conversion/distance'
 
-def test_sanity
-  assert_equal to_meter[:inches][1000], 25.4
+class DistanceTest < Minitest::Test
+  def test_sanity
+    assert_equal to_meter[:inches][1000], 25.4
+  end
 end
