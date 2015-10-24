b9b7ee3caa4b442d6cc7bb8fd1104c5a0d35f26b
Victor Goff <keeperotphones@gmail.com>
2015-10-23 22:19:16 -0400

    Organizing namespace and flesh out API
    We are still experiementing with what the tool should be.  And how to
    use it.  This experimenting with a module and class combination.

--- spec/distance_spec.rb
+++ spec/distance_spec.rb
@@ -1,8 +1,18 @@
 require 'spec_helper'
-require 'unit_conversion/distance'
 
-class DistanceTest < Minitest::Test
-  def test_sanity
-    assert_equal to_meter[:inches][1000], 25.4
+include Convertor
+
+describe Convertor::Distance do
+  before do
+    @my_distance = Distance.new.setup(1000, 'inch')
+  end
+
+  it 'must be 25.4 neters fir 1k inches' do
+    @my_distance.to_inch.must_equal 1000
+  end
+
+  it 'must be able to convert to a float' do
+    @my_distance.to_f.must_equal 25.4
   end
 end
+
