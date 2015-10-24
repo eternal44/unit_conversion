fa10a45654f9c8dea943357a39d375f1f644121a
Victor Goff <keeperotphones@gmail.com>
2015-10-23 22:19:16 -0400

    Organizing namespace and flesh out API
    We are still experiementing with what the tool should be.  And how to
    use it.  This experimenting with a module and class combination.

--- lib/unit_conversion/distance.rb
+++ lib/unit_conversion/distance.rb
@@ -1,9 +1,44 @@
-module Distance
-  meter_to_inch = Proc.new do |n|
-    n * 0.0254
+module Convertor
+
+  class Distance
+    attr_reader :value
+
+    def setup(value, unit)
+      @value = value
+      send("from_#{unit}")
+      self
+    end
+    def to_meter
+      @unit = "meter"
+      @value
+      self
+    end
+
+    def from_meter
+      self
+    end
+
+    def from_inch
+      @unit = 'meter'
+      @value = proc do |n|
+        n * 0.0254
+      end .call(@value)
+    end
+
+    def to_inch
+      @unit = 'inch'
+      @value = ->(n) { n / 0.0254 }.call(@value)
+    end
+
+    def to_s
+      [@value, @unit].join(' ')
+    end
+    def to_f
+      to_s.to_f
+    end
+
+     def inspect # do we need this?
+       "#{self.class} object, with the current unit of #{@unit} and a value of #@value"
+     end
   end
-
-  to_meter = { inches: meter_to_inch }
-  # puts to_meter[:inches].call( 1000 )
 end
-
