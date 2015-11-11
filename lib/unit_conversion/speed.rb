7942578bf8a304c5bb34825ad91bac1c5cfc678b
James Youn <jamesyoun710@gmail.com>
2015-11-11 00:37:18 -0800

    Complete speed conversion methods & tests
    Using simpler test names to minimize our naming convention & to provide
    a succinct way to recognize a test in a long line of test suites.
    
    Also using one-line syntax for 'from' conversions for syntactic sugar.

--- lib/unit_conversion/speed.rb
+++ lib/unit_conversion/speed.rb
@@ -1,69 +1,65 @@
-module SpeedConversion
+module SpeedConvertor
   # Proxy = Meter per Second
 
-  def to_meter_per_second
-    case @unit
-    when 'mile_per_second'
-      @measurement *= 0.3048 * 5280
-    when 'mile_per_hour'
-      @measurement *= 0.44704
-    when 'kilometer_per_hour'
-      @measurement *= 0.277778
-    when 'knot'
-      @measurement *= 0.514444
-    when 'kilometer_per_second'
-      @measurement *= 1000.0
-    end
-  end
+  class Speed
+    attr_reader :value
 
-  def to_kilometer_per_second
-    if @unit == 'meter_per_second'
-      @measurement /= 1000
-    else
-      self.to_meter_per_second
-      @unit = 'meter_per_second'
-      self.to_kilometer_per_second
+    def initialize(value, unit)
+      @value = value
+      send("from_#{unit}")
+      self
     end
-  end
 
-  def to_kilometer_per_hour
-    if @unit == 'meter_per_second'
-      @measurement *= 3.6
-    else
-      self.to_meter_per_second
-      @unit = 'meter_per_second'
-      self.to_kilometer_per_hour
+    def from_kilometers_per_hour
+      @unit = 'kilometer per hour'
+      @value = ->(n) { n * 0.277778}.call(@value)
     end
-  end
 
-  def to_knot
-    if @unit == 'meter_per_second'
-      @measurement *= 1.94384449
-    else
-      self.to_meter_per_second
-      @unit = 'meter_per_second'
-      self.to_knot
+    def from_kilometers_per_second
+      @unit = 'kilometer per second'
+      @value = ->(n) { n * 1000.0 }.call(@value)
     end
-  end
 
-  def to_mile_per_second
-    if @unit == 'meter_per_second'
-      @measurement *= 2.23693629 / 60 / 60
-    else
-      self.to_meter_per_second
-      @unit = 'meter_per_second'
-      self.to_mile_per_second
+    def from_miles_per_hour
+      @unit = 'kilometer per second'
+      @value = ->(n) { n * 0.44704 }.call(@value)
     end
-  end
 
-  def to_mile_per_hour
-    if @unit == 'meter_per_second'
-      @measurement *= 2.23693629
-    else
-      self.to_meter_per_second
-      @unit = 'meter_per_second'
-      self.to_mile_per_hour
+    def from_knot
+      @unit = 'knot'
+      @value = ->(n) { n * 0.514444 }.call(@value)
     end
-  end
 
+    def from_meters_per_second
+      @unit = 'meters per second'
+      @value
+    end
+
+    def to_miles_per_hour
+      @unit = 'miles per hour'
+      @value = ->(n) { n * 2.23693629 }.call(@value)
+    end
+
+    def to_kilometers_per_second
+      @unit = 'kilometers per second'
+      @value = ->(n) { n / 1000.0 }.call(@value)
+    end
+
+    def to_knot
+      @unit = 'knot'
+      @value = ->(n) { n * 1.94384449 }.call(@value)
+    end
+
+    def to_meters_per_second
+      @unit = 'meters per second'
+      @value
+    end
+
+    def to_kilometers_per_hour
+      @unit = 'kilometers per hour'
+      @value = ->(n) { n * 3.6 }.call(@value)
+    end
+
+  end
 end
+
